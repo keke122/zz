a6c2168cadd8502050f39673125f8025463b3757
keke <284660540@qq.com>
2019-12-18 10:37:05 +0800

    init add

---
+++ git.py
@@ -0,0 +1,12 @@
+# 1. 配置用户名
+git config --global user.name keke
+# 2. 配置邮箱
+git config --global user.email 284660540@qq.com
+# 3.查看配置文件位置
+cat .gitconfig
+
+# 4.列表查看配置结果
+git config --list
+
+tarena@tedu:~/201911学习资料/month2019_12$ git init
+已初始化空的 Git 仓库于 /home/tarena/201911学习资料/month2019_12/.git/
